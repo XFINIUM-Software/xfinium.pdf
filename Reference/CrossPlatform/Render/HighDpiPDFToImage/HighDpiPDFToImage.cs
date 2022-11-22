1bd7619fc2fe82f4c544e5ad1c49e5c300fdb6d5
XFINIUM.PDF Support <support@xfiniumpdf.com>
2022-11-22 12:29:24 +0200

    Added HighDpiPDFToImage sample

---
+++ Reference/CrossPlatform/Render/HighDpiPDFToImage/HighDpiPDFToImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Xfinium.Pdf.Rendering;
+
+namespace Xfinium.Pdf.Samples
+{
+    class HighDpiPDFToImage
+    {
+        static void Main(string[] args)
+        {
+            FileStream pdfStream = File.OpenRead("..\\..\\..\\..\\..\\..\\SupportFiles\\xfinium.pdf");
+            PdfFixedDocument document = new PdfFixedDocument(pdfStream);
+            pdfStream.Dispose();
+
+            PdfPageRenderer pageRenderer = new PdfPageRenderer(document.Pages[0]);
+
+            PdfRendererSettings settings = new PdfRendererSettings(5200, 5200);
+            settings.RenderingSurface = pageRenderer.CreateRenderingSurface<PdfArgbStripRenderingSurface<int>>(settings.DpiX, settings.DpiY);
+
+            // Output will be a 32bit 60840x43160 pixels RGBA TIFF
+            FileStream pngStream = File.Create("XFINIUM.PDF.Page.0.tiff");
+            pageRenderer.ConvertPageToImage(pngStream, PdfPageImageFormat.Tiff, settings);
+            pngStream.Flush();
+            pngStream.Close();
+            
+            Console.WriteLine("Page 0 converted.");
+        }
+    }
+}
---
+++ Reference/CrossPlatform/Render/HighDpiPDFToImage/README.md
@@ -0,0 +1,2 @@
+The `HighDpiPDFToImage` sample shows how to convert a PDF page to a very large TIFF image using a strip based rendering surface.
+At 5200dpi the output image is 60840x43160 pixels, 32bpp. Its size is 10GB.
